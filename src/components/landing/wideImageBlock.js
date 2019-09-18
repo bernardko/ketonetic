1522434df56eee3f9f4d13ab40bbf1d70469fd12
Bernard Ko <bernardko@gmail.com>
2019-09-18 12:41:44 -0400

    Fixed image centering

--- src/components/landing/wideImageBlock.js
+++ src/components/landing/wideImageBlock.js
@@ -4,7 +4,9 @@ import Img from "gatsby-image"
 const WideImageBlock = ({blockData}) => (
     <section id={blockData.value.section_id} class={"m-t-lg " + blockData.value.section_classes}>
         <div className="col-lg-12 text-center wow zoomIn">
-            <Img alt={blockData.image.alt} fluid={blockData.image.imageFile.childImageSharp.fluid} className="img-fluid wide-image" />
+            <div className="img-fluid wide-image" style={{margin:"0 auto"}}>
+                <Img alt={blockData.image.alt} fluid={blockData.image.imageFile.childImageSharp.fluid} />
+            </div>
         </div>
     </section>
 )

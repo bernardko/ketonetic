0eba9f1ce380ac3ef8f452b659ca501023e39336
Bernard Ko <bernardko@gmail.com>
2019-11-14 20:09:58 -0500

    Fixed error when image is undefined

--- src/components/landing/stackedFeatureListBlock.js
+++ src/components/landing/stackedFeatureListBlock.js
@@ -13,7 +13,7 @@ const StackedFeatureListBlock = ({blockData}) => (
         {(blockData.value.features || []).map((feature, index) => {
             return (
                 <div class="row features-block">
-                    <div class="col-lg-6 features-text wow fadeInLeft">
+                    <div class={blockData.features[index].image ? "col-lg-12 features-text wow fadeInLeft" :"col-lg-6 features-text wow fadeInLeft"}>
                         {feature.highlight_tag && (
                             <small>{feature.highlight_tag}</small>
                         )}
@@ -23,11 +23,13 @@ const StackedFeatureListBlock = ({blockData}) => (
                             <a href={feature.link_url} class="btn btn-primary">{feature.link_text}</a>
                         )}
                     </div>
+                    {blockData.features[index].image ? blockData.features[index].image.imageFile ? (
                     <div class="col-lg-6 text-right wow fadeInRight">
                         {blockData.features[index].image && (
                             <Img fluid={blockData.features[index].image.imageFile.childImageSharp.fluid} alt={blockData.features[index].image.alt} class="img-fluid float-right" />
                         )}
                     </div>
+                    ):null}
                 </div>
             )
         })}

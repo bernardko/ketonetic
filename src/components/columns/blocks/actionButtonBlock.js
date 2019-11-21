0bdc7fd47d50a586bfacd794d016b6f36b3606ad
Bernard Ko <bernardko@gmail.com>
2019-11-20 23:25:13 -0500

    Fixed Link

--- src/components/columns/blocks/actionButtonBlock.js
+++ src/components/columns/blocks/actionButtonBlock.js
@@ -6,7 +6,12 @@ const ActionButtonBlock = ({blockData}) => (
     <div class="row">
         <div class={blockData.value.align_center ? "col-lg-12 wow fadeInLeft text-center" : "col-lg-12 wow fadeInLeft"}>
             <p>{blockData.value.description}</p>
-            <a href={blockData.value.button_url} class="btn btn-primary btn-rounded">{blockData.value.button_text}</a>    
+            {blockData.value.button_url && blockData.value.button_text && blockData.value.button_url.startsWith("/") && (
+                <Link to={blockData.value.button_url} class="btn btn-primary btn-rounded">{blockData.value.button_text}</Link>
+            )}
+            {blockData.value.button_url && blockData.value.button_text && blockData.value.button_url.startsWith("http") && (
+                <a href={blockData.value.button_url} class="btn btn-primary btn-rounded">{blockData.value.button_text}</a>
+            )} 
         </div>
     </div>
 )

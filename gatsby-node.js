e642d0417a041d949fad1a3009bb0eb27b8aa7a5
Bernard Ko <bernardko@gmail.com>
2020-01-17 00:02:22 -0500

    Added redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,7 +10,7 @@ const path = require(`path`)
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+    const { createPage, createRedirect } = actions
 
     return graphql(`
       {
@@ -26,35 +26,56 @@ exports.createPages = ({ graphql, actions }) => {
             specificPageType
             pageUrl
           }
+          redirects {
+            oldPath,
+            link
+          }
         }
       }
       
     `).then(result => {
         const processPages = page => {
-            const appLabel = page.specificPageType.split(".")[0]
-            var characterList = page.specificPageType.split(".")[1].split("")
-            const firstLetter = characterList.shift().toLowerCase()
-            const pageTemplate = path.resolve("./src/templates/" + appLabel + "/" + firstLetter + characterList.join("") + ".js")
-            createPage({
-                // Path for this page — required
-                path: page.pageUrl,
-                component: pageTemplate,
-                context: {
-                  // Add optional context data to be inserted
-                  // as props into the page component..
-                  //
-                  // The context data can also be used as
-                  // arguments to the page GraphQL query.
-                  //
-                  // The page "path" is always available as a GraphQL
-                  // argument.
-                  slug: page.slug,
-                },
-            })
+          const appLabel = page.specificPageType.split(".")[0]
+          var characterList = page.specificPageType.split(".")[1].split("")
+          const firstLetter = characterList.shift().toLowerCase()
+          const pageTemplate = path.resolve("./src/templates/" + appLabel + "/" + firstLetter + characterList.join("") + ".js")
+          createPage({
+              // Path for this page — required
+              path: page.pageUrl,
+              component: pageTemplate,
+              context: {
+                // Add optional context data to be inserted
+                // as props into the page component..
+                //
+                // The context data can also be used as
+                // arguments to the page GraphQL query.
+                //
+                // The page "path" is always available as a GraphQL
+                // argument.
+                slug: page.slug,
+              },
+          })
+        }
+
+        const processRedirects = redirect => {
+          createRedirect({
+            fromPath: redirect.oldPath,
+            isPermanent: true,
+            redirectInBrowser: true,
+            toPath: redirect.link
+          })
+          createRedirect({
+            fromPath: redirect.oldPath + "/",
+            isPermanent: true,
+            redirectInBrowser: true,
+            toPath: redirect.link
+          })
+
         }
 
         result.data.wagtail.landingPages.forEach(processPages)
         result.data.wagtail.categoryPages.forEach(processPages)
+        result.data.wagtail.redirects.forEach(processRedirects)
     })
 
 }

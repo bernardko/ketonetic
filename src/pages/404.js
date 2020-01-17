a3083ba1ac60b183626e267d3ec5bf2343d90cc9
Bernard Ko <bernardko@gmail.com>
2020-01-17 00:49:39 -0500

    Fixed issue with 404 page flash

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,18 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <section class="middle-box text-center animated fadeInDown">
-      <h1>404</h1>
-      <h3 class="font-bold">Page Not Found</h3>
-      <div class="error-desc">
-        Sorry, this doesn't seem to exist. Try navigating the site from the main menu at the top of the page.
-      </div>
-      <Link to="/" className="btn btn-primary m-t-xl">Return to Home Page</Link>
-    </section>
-  </Layout>
-)
+// Fix for 404 flash on redirect (https://github.com/gatsbyjs/gatsby/issues/5329)
+const browser = typeof window !== "undefined" && window;
+
+const NotFoundPage = () => {
+  return browser && (
+    <Layout>
+      <SEO title="404: Not found" />
+      <section class="middle-box text-center animated fadeInDown">
+        <h1>404</h1>
+        <h3 class="font-bold">Page Not Found</h3>
+        <div class="error-desc">
+          Sorry, this doesn't seem to exist. Try navigating the site from the main menu at the top of the page.
+        </div>
+        <Link to="/" className="btn btn-primary m-t-xl">Return to Home Page</Link>
+      </section>
+    </Layout>
+  )
+}
 
 export default NotFoundPage

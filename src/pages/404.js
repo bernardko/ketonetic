2990d53b83a2096cd12c15eac99949078095d81f
Bernard Ko <bernardko@gmail.com>
2020-01-17 00:54:02 -0500

    Fixed 404 flash color

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,17 +8,19 @@ import SEO from "../components/seo"
 const browser = typeof window !== "undefined" && window;
 
 const NotFoundPage = () => {
-  return browser && (
+  return (
     <Layout>
       <SEO title="404: Not found" />
-      <section class="middle-box text-center animated fadeInDown">
-        <h1>404</h1>
-        <h3 class="font-bold">Page Not Found</h3>
-        <div class="error-desc">
-          Sorry, this doesn't seem to exist. Try navigating the site from the main menu at the top of the page.
-        </div>
-        <Link to="/" className="btn btn-primary m-t-xl">Return to Home Page</Link>
-      </section>
+      {browser && (
+        <section class="middle-box text-center animated fadeInDown">
+          <h1>404</h1>
+          <h3 class="font-bold">Page Not Found</h3>
+          <div class="error-desc">
+            Sorry, this doesn't seem to exist. Try navigating the site from the main menu at the top of the page.
+          </div>
+          <Link to="/" className="btn btn-primary m-t-xl">Return to Home Page</Link>
+        </section>
+      )}
     </Layout>
   )
 }
